ecaad7d50b5cc3ed9b3727a4b421fab8bed28d6f
Keith Duke <2253222+keithduke@users.noreply.github.com>
2022-05-10 12:16:32 -0500

    Narrow

--- bunny/src/main.js
+++ bunny/src/main.js
@@ -6,7 +6,7 @@ import GameOver from './scenes/GameOver.js';
 
 export default new Phaser.Game({
   type: Phaser.AUTO,
-  width: 480,
+  width: 360,
   height: 640,
   scene: [Game, GameOver],
   physics: {
